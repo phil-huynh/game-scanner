51cb30427e9fc4e01e2cc3cc487edd162b3aa5da
phil-huynh <philip.huynh@galvanize.com>
2024-03-07 01:50:42 -0500

    added titles to table data

--- frontend/src/GameDataDisplay.jsx
+++ frontend/src/GameDataDisplay.jsx
@@ -10,14 +10,28 @@ export default function GameDataDisplay() {
       <div>
         <Menu/>
       </div>
-
       <div>
         <h1>{`${currentGame.productName ? currentGame.productName : "DATA UNAVAILABLE"}`}</h1>
-        <h4>{`${currentGame.consoleName ? currentGame.consoleName : "DATA UNAVAILABLE"}`}</h4>
-        {/* <h3>{`${currentGame.genre ? currentGame.genre : "DATA UNAVAILABLE"}`}</h3>
-        <h5>{`asin: ${currentGame.asin ? currentGame.asin : "DATA UNAVAILABLE"}`}</h5>
-        <h5>{`epid: ${currentGame.epid ? currentGame.epid : "DATA UNAVAILABLE"}`}</h5>
-        <h5>{`upc: ${currentGame.upc ? currentGame.upc : "DATA UNAVAILABLE"}`}</h5> */}
+        <div className="tableContainer">
+          <table>
+            <tr>
+              <td title="	The name of the console on which the item was released">Console</td>
+              <td>{`${currentGame.consoleName ? currentGame.consoleName : "DATA UNAVAILABLE"}`}</td>
+            </tr>
+            <tr>
+              <td title="Game upc">upc</td>
+              <td>{currentGame.upc ? currentGame.upc : "DATA UNAVAILABLE"}</td>
+            </tr>
+            <tr>
+              <td title="Unique identifier (ASIN) for this product on Amazon.com">asin</td>
+              <td>{currentGame.asin ? currentGame.asin : "DATA UNAVAILABLE"}</td>
+            </tr>
+            <tr>
+              <td title="Unique identifier (ePID) for this product on eBay">epid</td>
+              <td>{currentGame.epid ? currentGame.epid : "DATA UNAVAILABLE"}</td>
+            </tr>
+          </table>
+        </div>
       </div>
       <section className="dataSection">
         <h2>Price Charting</h2>
@@ -28,23 +42,23 @@ export default function GameDataDisplay() {
               <td className="tableHeader">Price</td>
             </tr>
             <tr>
-              <td>New</td>
+              <td title="Price for item with original packaging and original seal">New</td>
               <td>{currentGame.newPrice ? currentGame.newPrice : "DATA UNAVAILABLE"}</td>
             </tr>
             <tr>
-              <td>Complete in Box</td>
+              <td title="Price for item only with original box and manual included">Complete in Box</td>
               <td>{currentGame.cibPrice ? currentGame.cibPrice : "DATA UNAVAILABLE"}</td>
             </tr>
             <tr>
-              <td>Graded</td>
+              <td title="Price for brand new item graded by WATA or VGA">Graded</td>
               <td>{currentGame.gradedPrice ? currentGame.gradedPrice : "DATA UNAVAILABLE"}</td>
             </tr>
             <tr>
-              <td>Loose</td>
+              <td title="Price for item only without box or manual">Loose</td>
               <td>{currentGame.loosePrice ? currentGame.loosePrice : "DATA UNAVAILABLE"}</td>
             </tr>
             <tr>
-              <td>Box Only</td>
+              <td title="Price for the original box only">Box Only</td>
               <td>{currentGame.boxOnlyPrice ? currentGame.boxOnlyPrice : "DATA UNAVAILABLE"}</td>
             </tr>
           </table>
@@ -59,11 +73,11 @@ export default function GameDataDisplay() {
               <td className="tableHeader">Price</td>
             </tr>
             <tr>
-              <td>Loose</td>
+              <td title="The price that GameStop charges for this game in Pre-Owned condition.">Sell</td>
               <td>{currentGame.gamestopPrice ? currentGame.gamestopPrice : "DATA UNAVAILABLE"}</td>
             </tr>
             <tr>
-              <td>Box Only</td>
+              <td title="The Trade price is what GameStop pays in cash for trade-in">Trade Price</td>
               <td>{currentGame.gamestopTradePrice ? currentGame.gamestopTradePrice : "DATA UNAVAILABLE"}</td>
             </tr>
           </table>
@@ -79,17 +93,17 @@ export default function GameDataDisplay() {
               <td className="tableHeader">Sell Price</td>
             </tr>
             <tr>
-              <td>New</td>
+              <td title="The recommended price for retailers buying/selling in brand new condition">New</td>
               <td>{currentGame.retailNewBuy ? currentGame.retailNewBuy : "DATA UNAVAILABLE"}</td>
               <td>{currentGame.retailNewSell ? currentGame.retailNewSell : "DATA UNAVAILABLE"}</td>
             </tr>
             <tr>
-              <td>Complete in Box</td>
+              <td title="The recommended price for retailers buying/selling in complete in box condition">Complete in Box</td>
               <td>{currentGame.retailCibBuy ? currentGame.retailCibBuy : "DATA UNAVAILABLE"}</td>
               <td>{currentGame.retailCibSell ? currentGame.retailCibSell : "DATA UNAVAILABLE"}</td>
             </tr>
             <tr>
-              <td>Loose</td>
+              <td title="The recommended price for retailers buying/selling in loose condition">Loose</td>
               <td>{currentGame.retailLooseBuy ? currentGame.retailLooseBuy : "DATA UNAVAILABLE"}</td>
               <td>{currentGame.retailLooseSell ? currentGame.retailLooseSell : "DATA UNAVAILABLE"}</td>
             </tr>
